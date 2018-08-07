0a8fb63b3ef587e6cae258054880db35461b4156
Yubin <ablacktshirt@gmail.com>
2018-08-07 15:58:29 +0800

    Update useful_scipt.sh

--- useful_scipt.sh
+++ useful_scipt.sh
@@ -7,3 +7,10 @@ fuck_cscope() {
 git_track_all_remote_branch() {
     for i in $(git branch -r | grep -vE "HEAD|master"); do git branch --track ${i#*/} $i; done
 }
+
+install_shadowsocks_libev() {
+    apt-get install software-properties-common -y
+    add-apt-repository ppa:max-c-lv/shadowsocks-libev -y
+    apt-get update
+    apt install shadowsocks-libev -y
+}
